373b8b139089e62a8c77054c89fe5e1fa323184c
Manuzz64 <zz64manu@gmail.com>
2025-01-01 20:01:02 +0000

    hamburgah mewnu

--- script.js
+++ script.js
@@ -121,3 +121,8 @@ window.addEventListener('resize', () => {
         button.innerHTML = '<i class="fas fa-play"></i> Play Music';
     }
 }});
+
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+}
